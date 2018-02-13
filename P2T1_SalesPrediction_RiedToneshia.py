5fd098b6775ed0b7d67d980ec96d271b5aa65afc
princess2597 <36427271+princess2597@users.noreply.github.com>
2018-02-13 00:13:15 +0000

    Add files via upload

---
+++ P2T1_SalesPrediction_RiedToneshia.py
@@ -0,0 +1,12 @@
+#Get the projected total sales
+total_sales = float(input('Enter the projected sales:'))
+#Calculatethe profit as 23 percent of total sales
+annualprofit = total_sales* .23
+#Display the profit
+print('The profit is $',format(annualprofit,',.2f'))
+
+#CTI-110
+#P2T1 - Sales Prediction
+#Toneshia Ried
+#February 12, 2018
+
