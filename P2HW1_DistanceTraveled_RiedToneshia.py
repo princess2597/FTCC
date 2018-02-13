969125f13baffe12f68933bdcf19f32ca4f8ee49
princess2597 <36427271+princess2597@users.noreply.github.com>
2018-02-13 23:10:43 +0000

    Add files via upload

---
+++ P2HW1_DistanceTraveled_RiedToneshia.py
@@ -0,0 +1,9 @@
+#CTI - 110
+#P2HW1 - Distance Traveled
+#Toneshia Ried
+#February 13, 2018
+
+speed = 70
+print("In 6 hours the car will travel",format(( speed * 6 )), "miles" )
+print("In 10 hours the car will travel",format(( speed * 10 )), "miles" )
+print("In 15 hours the car will travel",format(( speed * 15 )), "miles" )
